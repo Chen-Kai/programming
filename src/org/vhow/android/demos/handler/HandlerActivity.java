b861cc6d7d36beaf32999381495d1e32aa9dab25
DailyEfforts <DailyEfforts@gmail.com>
2012-07-02 22:01:08 +0800

    Add a cancel button to removeMessages

--- src/org/vhow/android/demos/handler/HandlerActivity.java
+++ src/org/vhow/android/demos/handler/HandlerActivity.java
@@ -1,7 +1,6 @@
 package org.vhow.android.demos.handler;
 
 import org.vhow.android.demos.R;
-import org.vhow.android.demos.handler.HandlerActivity.MsgHandler;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -14,8 +13,9 @@ import android.widget.EditText;
 public class HandlerActivity extends Activity {
 	private Handler mHandler;
 	private EditText mEditText;
-	private Button mButton;
+	private Button mButtonPost;
 	private MsgHandler mMsgHnadler;
+	private Button mButtonCancel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +26,24 @@ public class HandlerActivity extends Activity {
 		mMsgHnadler = new MsgHandler();
 
 		mEditText = (EditText) findViewById(R.id.editText1_handler);
-		mButton = (Button) findViewById(R.id.button1_handler);
+		mButtonPost = (Button) findViewById(R.id.btn_post_handler);
+		mButtonCancel = (Button) findViewById(R.id.btn_cancel_handler);
 
-		mButton.setOnClickListener(new View.OnClickListener() {
-
+		mButtonPost.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				mHandler.post(mWorker);
 				mMsgHnadler.sendMessageDelayed(Message.obtain(mMsgHnadler,
-						MsgHandler.MSG_SET_CONTENT_TO_EDITTEXT), 3000);
+						MsgHandler.MSG_SET_CONTENT_TO_EDITTEXT), 1000);
 				mMsgHnadler.sendMessageDelayed(Message.obtain(mMsgHnadler,
-						MsgHandler.MSG_SET_CONTENT_TO_EDITTEXT_ANOTHER), 5000);
+						MsgHandler.MSG_SET_CONTENT_TO_EDITTEXT_ANOTHER), 6000);
+			}
+		});
+		
+		mButtonCancel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mMsgHnadler.removeMessages(MsgHandler.MSG_SET_CONTENT_TO_EDITTEXT_ANOTHER);
 			}
 		});
 	}
@@ -57,7 +64,6 @@ public class HandlerActivity extends Activity {
 			switch (msg.what) {
 			case MSG_SET_CONTENT_TO_EDITTEXT:
 				mEditText.setText("handle MSG_SET_CONTENT_TO_EDITTEXT");
-				mHandler.removeMessages(MSG_SET_CONTENT_TO_EDITTEXT_ANOTHER);
 				break;
 			case MSG_SET_CONTENT_TO_EDITTEXT_ANOTHER:
 				mEditText.setText("handle MSG_SET_CONTENT_TO_EDITTEXT_ANOTHER");
