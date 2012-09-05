e536f5ed5f6a25dca10e8af03078e39ad8de33fe
DailyEfforts <DailyEfforts@gmail.com>
2012-09-05 23:10:53 +0800

    add popup window

---
+++ src/org/vhow/android/basic/popup/MyPopupWindow.java
@@ -0,0 +1,34 @@
+package org.vhow.android.basic.popup;
+
+import org.vhow.android.basic.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+
+public class MyPopupWindow extends PopupWindow {
+
+	private Context mContext;
+	private View mView;
+
+	public MyPopupWindow(Context context) {
+		super(context);
+		mContext = context;
+		LayoutInflater inflater = (LayoutInflater) mContext
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mView = inflater.inflate(R.layout.my_popup_window_ui, null, false);
+
+		setContentView(mView);
+		
+this.setFocusable(true);
+
+//		this.setInputMethodMode(INPUT_METHOD_NEEDED);
+//		this.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+		// this.setSoftInputMode(mode)
+
+	}
+
+}
