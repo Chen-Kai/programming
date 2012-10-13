c0485003e34f5e454a064257ebb227270a43044d
DailyEfforts <DailyEfforts@gmail.com>
2012-10-13 11:15:06 +0800

    refined the code format of SumCounter.java

--- app/src/org/vhow/algrithm/sum/SumCounter.java
+++ app/src/org/vhow/algrithm/sum/SumCounter.java
@@ -3,6 +3,7 @@ package org.vhow.algrithm.sum;
 public class SumCounter {
 
 	public static long getSumStupidMethod(final int n) {
+		
 		int sum = 0;
 		
 		for (int i = 1; i <= n; i++) {
@@ -10,13 +11,16 @@ public class SumCounter {
 		}
 
 		return sum;
+		
 	}
 
 	public static long getSumSmarterMethod(final int n) {
+		
 		int sum = 0;
 
-		sum = (int) ((1 + n) / 2.0F * n);
+		sum = (1 + n) * n / 2;
 
 		return sum;
+		
 	}
 }
