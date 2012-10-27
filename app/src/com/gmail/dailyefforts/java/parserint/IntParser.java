b045f4925be01eac3f297b54a2d4d83c6388977c
DailyEfforts <DailyEfforts@gmail.com>
2012-10-27 21:58:50 +0800

    format IntParser.java

--- app/src/com/gmail/dailyefforts/java/parserint/IntParser.java
+++ app/src/com/gmail/dailyefforts/java/parserint/IntParser.java
@@ -1,10 +1,13 @@
 package com.gmail.dailyefforts.java.parserint;
 
 public class IntParser {
+	
 	public static int parseHexToInt(String hexString) {
 		return Integer.parseInt(hexString, 16);
 	}
+
 	public static int decode(String str) {
 		return Integer.decode(str);
 	}
+	
 }
