05068dd42a5d98efcad363ce97308ac35c9ccbad
DailyEfforts <DailyEfforts@gmail.com>
2012-10-28 23:54:09 +0800

    analyse drawText method

---
+++ app/src/com/gmail/dailyefforts/android/basic/text/DrawTextActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.dailyefforts.android.basic.text;
+
+import com.gmail.dailyefforts.android.basic.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawTextActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.draw_text_activity_ui);
+	}
+}
