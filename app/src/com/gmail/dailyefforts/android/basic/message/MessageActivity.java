a2f939262a5efb6d7816a895af9982d7c9a6e064
DailyEfforts <DailyEfforts@gmail.com>
2013-01-12 00:52:15 +0800

    Add the method to save the sent msg to "sent" box.

---
+++ app/src/com/gmail/dailyefforts/android/basic/message/MessageActivity.java
@@ -0,0 +1,41 @@
+package com.gmail.dailyefforts.android.basic.message;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+
+import com.gmail.dailyefforts.android.basic.R;
+
+public class MessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_message);
+	}
+
+	public void sendMsg(View view) {
+		
+		SmsManager smsManager = SmsManager.getDefault();
+		
+		String address = "address here";
+		String text = "text message here";
+		
+		// Send text message to the address.
+		smsManager.sendTextMessage(address, null, text, null, null);
+		
+		Uri uri = Uri.parse("content://sms/sent/");
+		
+		ContentValues values = new ContentValues();
+		values.put("address", address);
+		values.put("body", text);
+		
+		// Insert the sent message to the "sent" box 
+		getContentResolver().insert(uri, values );
+		
+	}
+
+}
