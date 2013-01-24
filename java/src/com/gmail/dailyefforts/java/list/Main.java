459d1b76e9227fd4fce0c6d414d621c95da2cbcc
DailyEfforts <DailyEfforts@gmail.com>
2013-01-24 22:13:04 +0800

    modify the structure of the file system.

---
+++ java/src/com/gmail/dailyefforts/java/list/Main.java
@@ -0,0 +1,11 @@
+package com.gmail.dailyefforts.java.list;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class Main {
+	ArrayList<String> mArrayList;
+	Vector<String> mVector;
+	public static void main(String[] args) {
+	}
+}
