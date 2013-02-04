dc982f25d4598039abd989685a2384b270e66481
DailyEfforts <DailyEfforts@gmail.com>
2013-02-04 19:17:00 +0800

    add Env.java to get env properties.

---
+++ java/src/com/gmail/dailyefforts/java/runtime/Env.java
@@ -0,0 +1,15 @@
+package com.gmail.dailyefforts.java.runtime;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class Env {
+	public static void main(String[] args) {
+		Map<String, String> envMap = System.getenv();
+		Collection<String> c = envMap.values();
+		for (String name : c) {
+			System.out.format("%s=%s%n", name, envMap.get(name));
+		}
+		
+	}
+}
