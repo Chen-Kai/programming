c655413369254695409c60be5dce4a9dfa78ac65
DailyEfforts <DailyEfforts@gmail.com>
2014-03-18 19:11:05 +0800

    update py example

---
+++ python/py-pjt/src/var.py
@@ -0,0 +1,21 @@
+print (3 + 7)
+
+print ('this is a string')
+
+print (type(3))
+
+# <type 'str'>
+print (type('str'))
+
+a = 3
+
+print a, type(a)
+
+print (2 << 1)
+
+if (a > 0):
+    print 'a > 0'
+elif (a < 0):
+    print 'a < 0'
+else:
+    print 'a == 0'
