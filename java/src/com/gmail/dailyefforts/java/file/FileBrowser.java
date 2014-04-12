bc362b2ad31844a64c379ba3c1cd7619f0f2173d
DailyEfforts <DailyEfforts@gmail.com>
2014-04-12 13:31:35 +0800

    Scan files in Java

--- java/src/com/gmail/dailyefforts/java/file/FileBrowser.java
+++ java/src/com/gmail/dailyefforts/java/file/FileBrowser.java
@@ -1,31 +1,39 @@
 package com.gmail.dailyefforts.java.file;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileBrowser {
-	private static Map<String, String> map = new HashMap<String, String>();
-
-	public static void main(String[] args) {
-		File root = new File("/");
-		map.clear();
-		walkThrough(root);
-
-		System.out.println(map.toString());
+	public static void main(String[] args) throws Exception {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				"./filelist.bak")));
+		File[] roots = File.listRoots();
+		if (roots != null) {
+			for (File root : roots) {
+				walkThrough(root, writer);
+			}
+		}
+		writer.close();
 	}
 
-	public static void walkThrough(File root) {
-		File[] files = root.listFiles();
-		if (files != null) {
-			for (File f : files) {
-				if (f.isFile()) {
-					System.out.println(f.getName());
-					map.put(f.getName(),
-							String.format("%.2f", f.length() / 1000.0F));
-				} else {
-					walkThrough(f);
+	public static void walkThrough(File file, BufferedWriter reader) {
+		File[] files = file.listFiles();
+		if (files == null) {
+			return;
+		}
+		for (File f : files) {
+			if (f.isFile()) {
+				try {
+					reader.write(f.getAbsolutePath() + "\t" + f.length() / 1024
+							+ " KB");
+					reader.newLine();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
+			} else {
+				walkThrough(f, reader);
 			}
 		}
 	}
