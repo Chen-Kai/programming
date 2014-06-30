1bd135046d48602d7ad894dae0f7dc7bfd2f2b63
DailyEfforts <DailyEfforts@gmail.com>
2014-07-01 00:01:53 +0900

    update

--- java/src/com/gmail/dailyefforts/java/array/ArrayDemo.java
+++ java/src/com/gmail/dailyefforts/java/array/ArrayDemo.java
@@ -3,18 +3,19 @@ package com.gmail.dailyefforts.java.array;
 import java.util.Arrays;
 
 public class ArrayDemo {
-	public static void main(String[] args) {
-		int [] a = {1, 2, 3};
-		int [] b = new int [3];
-		
-		// Assigns the specified int value to each element of the specified array of ints.
-		Arrays.fill(b, 0);
-	
-		System.out.println("ArrayDemo.main() " + Arrays.toString(b));
-		
-		// Creates an exact copy of an array.
-		b = a.clone();
-		
-		System.out.println("ArrayDemo.main() " + Arrays.toString(b));
-	}
+    public static void main(String[] args) {
+        int[] a = {2, 3, 1};
+        int[] b = new int[3];
+
+        // Assigns the specified int value to each element of the specified array of ints.
+        Arrays.fill(b, 0);
+        System.out.println(Arrays.toString(b));
+
+        // Creates an exact copy of an array.
+        b = a.clone();
+        System.out.println(Arrays.toString(b));
+
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
 }
