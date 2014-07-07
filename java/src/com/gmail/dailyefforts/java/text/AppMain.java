9952080095dd49f60224779a6718803f56b1423f
DailyEfforts <DailyEfforts@gmail.com>
2014-07-08 00:25:13 +0900

    Read text in utf-8 format.

--- java/src/com/gmail/dailyefforts/java/text/AppMain.java
+++ java/src/com/gmail/dailyefforts/java/text/AppMain.java
@@ -1,17 +1,34 @@
 package com.gmail.dailyefforts.java.text;
 
+import java.io.*;
+
 public class AppMain {
 
-	public static void main(String[] args) {
-
-		CharSequence a = "a";
-
-		// Get the unicode of 'a'
-		int unicode = Character.codePointAt(a, 0);
-
-		System.out.println("AppMain.main() unicode: 0x"
-				+ Integer.toHexString(unicode));
-
-	}
-
+    public static void main(String[] args) {
+        final CharSequence a = "a";
+        // 查看字符a的unicode值
+        final int unicode = Character.codePointAt(a, 0);
+        System.out.println("Unicode: 0x" + Integer.toHexString(unicode));
+        File file = new File(new File(".").getParent(), "zh_CN.txt");
+        System.out.println(file.getAbsoluteFile() + ", " + file.exists());
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println("line: " + line);
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
